4ac91536e2a93c0dbcc59d248beb4027d005b9e5
Sourcery AI <>
2022-08-02 19:30:33 +0000

    'Refactored by Sourcery'

--- examples/codes/plugin/across-languages/server/python/plugin_pb2_grpc.py
+++ examples/codes/plugin/across-languages/server/python/plugin_pb2_grpc.py
@@ -26,8 +26,6 @@ class PluginServicer(object):
   pass
 
   def Call(self, request, context):
-    # missing associated documentation comment in .proto file
-    pass
     context.set_code(grpc.StatusCode.UNIMPLEMENTED)
     context.set_details('Method not implemented!')
     raise NotImplementedError('Method not implemented!')
